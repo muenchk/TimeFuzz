ae7745c005df1cda1d6ae1b70965d6117074e762
Kai Münch <kai.muench@outlook.de>
2025-03-06 17:05:42 +0100

    Sources are now chosen from different delta debugging instances, to ensure as many pathes as possible are preserved

--- include/FilterFunctions.h
+++ include/FilterFunctions.h
@@ -5,6 +5,10 @@
 #include "Generation.h"
 #include "Oracle.h"
 #include "Settings.h"
+#include "DeltaDebugging.h"
+#include "Data.h"
+
+#include <set>
 
 class FilterFunctions
 {
@@ -12,6 +16,83 @@ public:
 	template <class T>
 	static void FilterSet(std::shared_ptr<SessionData> _sessiondata, std::shared_ptr<Generation> generation, std::set<std::shared_ptr<Input>, FormIDLess<Input>>& targets, double max, double frac, bool checkdeltaflag, bool primary, int targetval)
 	{
+		std::vector<std::shared_ptr<DeltaDebugging::DeltaController>> controllers;
+		generation->GetDDControllers(controllers);
+		auto itrus = controllers.begin();
+		// remove controllers without results
+		while (itrus != controllers.end()) {
+			if ((*itrus)->GetResults()->size() > 0) {
+				itrus++;
+			}
+			else
+				itrus = controllers.erase(itrus);
+		}
+		std::vector<int32_t> rootNums;
+		std::vector<int32_t> boundaries;
+		std::vector<std::vector<std::shared_ptr<Input>>> chosen;
+		chosen.resize(controllers.size());
+		rootNums.resize(controllers.size());
+		boundaries.resize(controllers.size());
+		for (size_t i = 0; i < controllers.size(); i++) {
+			rootNums[i] = 0;
+			boundaries[i] = 0;
+		}
+		int32_t idx = 0;
+		// distribute budget to controllers equally
+		std::set<size_t> fixed;
+		for (int32_t i = 0; i < targetval; i++) {
+			if (fixed.contains(idx)) {
+				idx++;
+			} else if (idx < (int32_t)controllers.size()) {
+				if (boundaries[idx] >= (int32_t)controllers[idx]->GetTestsTotal()) {
+					// if the boundary exceeds the number of inputs available for this root, fix the upper ceiling
+					fixed.insert(idx);
+					idx++;
+				} else {
+					boundaries[idx]++;
+					idx++;
+				}
+			}
+			if (idx == (int32_t)controllers.size())
+				idx = 0;
+			if (fixed.size() == controllers.size()) // if all roots are already fixed stop calculation boundaries #infiniteloops
+				break;
+		}
+
+		auto decBudget = [&idx, &boundaries, &chosen, &rootNums, &targets]() {
+			boundaries[idx]--;
+			// if now a root has exceeded the new budget, remove one of their inputs from the targets
+			if (rootNums[idx] > boundaries[idx])
+			{
+				targets.erase(chosen[idx].back());
+				chosen.pop_back();
+			}
+			idx--;
+			if (idx == -1)
+				idx = (int32_t)boundaries.size() - 1;
+		};
+
+		auto getRoot = [&controllers, &_sessiondata](std::shared_ptr<Input> input) {
+			auto traceBack = [&_sessiondata](std::shared_ptr<Input> input, FormID origID) {
+				auto tmp = input;
+				while (tmp && tmp->GetParentID() != 0)
+					if (tmp->GetParentID() == origID)
+						return true;
+					else
+						tmp = _sessiondata->data->LookupFormID<Input>(tmp->GetParentID());
+				return false;
+			};
+			for (size_t i = 0; i < controllers.size(); i++) {
+				if (controllers[i]->GetResults()->contains(input))
+					return (int32_t)i;
+				if (controllers[i]->GetOriginalInput()->GetFormID() == input->GetFormID())
+					return (int32_t)i;
+				if (traceBack(input, controllers[i]->GetOriginalInput()->GetFormID()))
+					return (int32_t)i;
+			}
+			return -1;
+		};
+
 		std::set<std::shared_ptr<Input>, T> inputs;
 		// if the max is 0 just run loop a single time since 0 is the minimal value for primaryScore
 		if (max == 0.0f)
@@ -33,8 +114,21 @@ public:
 							(*itr)->GetOracleResult() == OracleResult::Failing &&
 								(int32_t)(*itr)->Length() > _sessiondata->_settings->methods.IterativeConstruction_Backtrack_Backtrack_min) &&
 						(checkdeltaflag == false || (*itr)->HasFlag(Input::Flags::DeltaDebugged) == false)) {
-						targets.insert(*itr);
-						loginfo("Found New Source: {}", Input::PrintForm(*itr));
+						// check if the input has a dd source and whether the max alloted number has been reached
+						auto index = getRoot(*itr);
+						if (index == -1) {
+							targets.insert(*itr);
+							decBudget();
+							loginfo("Found New Source: {}", Input::PrintForm(*itr));
+						} else {
+							if (rootNums[index] < boundaries[index])
+							{
+								targets.insert(*itr);
+								chosen[index].push_back(*itr);
+								rootNums[index]++;
+								loginfo("Found New Source: {}", Input::PrintForm(*itr));
+							}
+						}
 					}
 				}
 				itr++;
